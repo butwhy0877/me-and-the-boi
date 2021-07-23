1206b67813f5ced620656213ddddc93256681941
butwhy0877 <66552597+butwhy0877@users.noreply.github.com>
2021-07-23 14:57:01 +0800

    Update concept.py

--- concept.py
+++ concept.py
@@ -1,13 +1,13 @@
 # stats mock-up 
 
 # temporary value
-very_low = (10)
-low = (31)
-medium_low = (51)
-medium = (61)
-medium_high = (71)
-high = (81)
-very_high = (101)
+very_low = (20)
+low = (40)
+medium_low = (60)
+medium = (80)
+medium_high = (100)
+high = (120)
+very_high = (140)
 
 # toughness=attack(physical)
 # fortitude=recovery+defense/confidence
@@ -29,11 +29,14 @@ print(aaron["wealth"])
 baaron = aaron["wealth"] - 50
 print(baaron)
 
-
+ 
 # ego system(bullshit system)
+# bullshit defense value ranges from (lowest)8-2(highest)
+# > avoids very high bullshit character from one shot-ting low value bullshitter
+# bullshit result times bullshit attack value is health points
+# bullshit health points plus 200 ego points
+# lowest possible value= 360
+# highest possible value= 480
+# *for ego system*(affected by charisma debuff)
 
-def bullshit():
-	ego_value = 1000
-	attack
-	defense
 	
