7ab0d6652fdebbc60b0d9cdfaf402cc6cabf3da5
butwhy0877 <66552597+butwhy0877@users.noreply.github.com>
2021-07-23 11:34:45 +0800

    the big brain idea
    conceptualization/mock-up?

---
+++ concept.py
@@ -0,0 +1,38 @@
+# stats mock-up 
+
+very_low = (10)
+low = (31)
+medium_low = (51)
+medium = (61)
+medium_high = (71)
+high = (81)
+very_high = (101)
+
+# toughness=attack(physical)
+# fortitude=recovery+defense/confidence
+# intelligence=counterattack/quick reflex(defense)
+# charisma=influencial power, support
+# wealth=money
+
+# dictionary of aaron's stats
+
+aaron = {
+	"toughness": medium_high,
+	"fortitude": medium_high,
+	"wealth": medium,
+	"charisma": medium_high,
+	"intelligence": medium
+}
+print(aaron)
+print(aaron["wealth"])
+baaron = aaron["wealth"] - 50
+print(baaron)
+
+
+# ego system(bullshit system)
+
+def bullshit():
+	ego_value = 1000
+	attack
+	defense
+	
